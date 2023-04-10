afa989aadd21e0e407661346e4b01dbab3dd6c0f
ton993t <ton993t@gmail.com>
2023-04-10 17:28:49 +0700

    cau3

---
+++ main/i2c_example_main.c
@@ -0,0 +1,320 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+/* i2c - Example
+
+   For other examples please check:
+   https://github.com/espressif/esp-idf/tree/master/examples
+
+   See README.md file to get detailed usage of this example.
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <string.h>
+#include "esp_log.h"
+#include "driver/i2c.h"
+#include "sdkconfig.h"
+#include "ssd1306.h"
+#include "font8x8_basic.h"
+#include "uit128x64.h"
+
+static const char *TAG = "i2c-example";
+
+#define _I2C_NUMBER(num) I2C_NUM_##num
+#define I2C_NUMBER(num) _I2C_NUMBER(num)
+
+
+#define I2C_MASTER_SCL_IO 4               /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 5               /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUMBER(0) /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ 100000        /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+
+#define OLED_ADDR OLED_I2C_ADDRESS   /*!< slave address for BH1750 sensor */
+
+#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
+
+#define GPIO_PULLUP_ENABLE 1
+#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0                       /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                             /*!< I2C ack value */
+#define NACK_VAL 0x1                            /*!< I2C nack value */
+
+void ssd1306_init() {
+	esp_err_t espRc;
+
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+
+	i2c_master_write_byte(cmd, OLED_CMD_SET_CHARGE_PUMP, true);
+	i2c_master_write_byte(cmd, 0x14, true);
+
+	i2c_master_write_byte(cmd, OLED_CMD_SET_SEGMENT_REMAP, true); // reverse left-right mapping
+	i2c_master_write_byte(cmd, OLED_CMD_SET_COM_SCAN_MODE, true); // reverse up-bottom mapping
+
+	i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_NORMAL, true);
+    i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_OFF, true);
+	i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_ON, true);
+	i2c_master_stop(cmd);
+
+	espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+	if (espRc == ESP_OK) {
+		ESP_LOGI(TAG, "OLED configured successfully");
+	} else {
+		ESP_LOGE(TAG, "OLED configuration failed. code: 0x%.2X", espRc);
+	}
+	i2c_cmd_link_delete(cmd);
+}
+
+void ssd1306_init_logo() {
+	esp_err_t espRc;
+
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+
+	i2c_master_write_byte(cmd, OLED_CMD_SET_CHARGE_PUMP, true);
+	i2c_master_write_byte(cmd, 0x14, true);
+
+	i2c_master_write_byte(cmd, OLED_CMD_SET_SEGMENT_REMAP, true); // reverse left-right mapping
+	i2c_master_write_byte(cmd, OLED_CMD_SET_COM_SCAN_MODE, true); // reverse up-bottom mapping
+
+	i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_NORMAL, true);  // normal mode 
+    i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_OFF, true);
+	i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_ON, true);
+	i2c_master_stop(cmd);
+
+	espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+	if (espRc == ESP_OK) {
+		ESP_LOGI(TAG, "OLED configured logo successfully");
+	} else {
+		ESP_LOGE(TAG, "OLED configuration logo failed. code: 0x%.2X", espRc);
+	}
+	i2c_cmd_link_delete(cmd);
+}
+
+void task_ssd1306_display_text(const void *arg_text) {
+	char *text = (char*)arg_text;
+	uint8_t text_len = strlen(text);
+
+	esp_err_t espRc;
+	
+	i2c_cmd_handle_t cmd;
+
+	uint8_t cur_page = 0;
+
+	cmd = i2c_cmd_link_create();
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+	i2c_master_write_byte(cmd, 0x00, true); // reset column - choose column --> 0
+	i2c_master_write_byte(cmd, 0x10, true); // reset line - choose line --> 0
+	i2c_master_write_byte(cmd, 0xB0 | cur_page, true); // reset page
+
+	i2c_master_stop(cmd);
+	
+	espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+	if (espRc == ESP_OK) {
+		ESP_LOGI(TAG, "OLED set up print successfully");
+	} else {
+		ESP_LOGE(TAG, "OLED set up print failed. code: 0x%.2X", espRc);
+	}
+	
+
+	i2c_cmd_link_delete(cmd);
+
+	for (uint8_t i = 0; i < text_len; i++) {
+		if (text[i] == '\n') {
+			cmd = i2c_cmd_link_create();
+			i2c_master_start(cmd);
+			i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+			i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+			i2c_master_write_byte(cmd, 0x00, true); // reset column
+			i2c_master_write_byte(cmd, 0x10, true);
+			i2c_master_write_byte(cmd, 0xB0 | ++cur_page, true); // increment page
+
+			i2c_master_stop(cmd);
+			
+			espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+			if (espRc == ESP_OK) {
+				ESP_LOGI(TAG, "OLED print 'enter' successfully");
+			} else {
+				ESP_LOGE(TAG, "OLED print 'enter'  failed. code: 0x%.2X", espRc);
+			}
+
+
+			i2c_cmd_link_delete(cmd);
+		} else {
+			cmd = i2c_cmd_link_create();
+			i2c_master_start(cmd);
+			i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+			i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+			i2c_master_write(cmd, font8x8_basic_tr[(uint8_t)text[i]], 8, true);
+
+			i2c_master_stop(cmd);
+			
+			espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);			
+			if (espRc == ESP_OK) {
+				ESP_LOGI(TAG, "OLED print successfully");
+			} else {
+				ESP_LOGE(TAG, "OLED print failed. code: 0x%.2X", espRc);
+			}
+			
+			i2c_cmd_link_delete(cmd);
+		}
+	}
+
+	vTaskDelete(NULL);
+}
+
+
+void task_ssd1306_display_logo(void * ignore) {
+
+	esp_err_t espRc;
+	
+	i2c_cmd_handle_t cmd;
+
+	uint8_t cur_page = 0;
+
+	cmd = i2c_cmd_link_create();
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+	i2c_master_write_byte(cmd, 0x00, true); // reset column - choose column --> 0
+	i2c_master_write_byte(cmd, 0x10, true); // reset line - choose line --> 0
+	i2c_master_write_byte(cmd, 0xB0 | cur_page, true); // reset page
+
+	i2c_master_stop(cmd);
+	
+	espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 100/portTICK_PERIOD_MS);
+	if (espRc == ESP_OK) {
+		ESP_LOGI(TAG, "OLED set up print logo successfully");
+	} else {
+		ESP_LOGE(TAG, "OLED set up print logo failed. code: 0x%.2X", espRc);
+	}
+	
+
+	i2c_cmd_link_delete(cmd);
+
+	for ( ; cur_page < 8; cur_page++) {
+		ESP_LOGI(TAG, "Curren page: %d", cur_page);
+		
+		if(cur_page != 0){
+			i2c_cmd_link_delete(cmd);
+		
+			cmd = i2c_cmd_link_create();
+			i2c_master_start(cmd);
+			i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+			i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+			i2c_master_write_byte(cmd, 0x00, true); // reset column
+			i2c_master_write_byte(cmd, 0x10, true);
+			i2c_master_write_byte(cmd, 0xB0 | cur_page, true); // increment page
+
+			i2c_master_stop(cmd);
+			
+			espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 50/portTICK_PERIOD_MS);
+			if (espRc == ESP_OK) {
+				ESP_LOGI(TAG, "OLED print 'enter' successfully");
+			} else {
+				ESP_LOGE(TAG, "OLED print 'enter'  failed. code: 0x%.2X", espRc);
+			}
+
+			i2c_cmd_link_delete(cmd);
+		}
+
+		cmd = i2c_cmd_link_create();
+		i2c_master_start(cmd);
+		i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+		i2c_master_write(cmd, logouit[cur_page], 128, true);
+
+		i2c_master_stop(cmd);
+			
+		espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 100/portTICK_PERIOD_MS);			
+		if (espRc == ESP_OK) {
+			ESP_LOGI(TAG, "OLED print successfully");
+		} else {
+			ESP_LOGE(TAG, "OLED print failed. code: 0x%.2X", espRc);
+		}			
+		
+	}
+
+	vTaskDelete(NULL);
+}
+
+
+
+
+void task_ssd1306_display_clear(void *ignore) {
+	i2c_cmd_handle_t cmd; 
+
+	uint8_t clear[128];
+	for (uint8_t i = 0; i < 128; i++) {
+		clear[i] = 0;
+	}
+	for (uint8_t i = 0; i < 8; i++) {
+		cmd = i2c_cmd_link_create();
+		i2c_master_start(cmd);
+		i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_SINGLE, true);
+		i2c_master_write_byte(cmd, 0xB0 | i, true);
+
+		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+		i2c_master_write(cmd, clear, 128, true);
+		i2c_master_stop(cmd);
+		i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+		i2c_cmd_link_delete(cmd);
+	}
+
+	vTaskDelete(NULL);
+}
+
+//============i2c master init================
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+        // .clk_flags = 0,          /*!< Optional, you can use I2C_SCLK_SRC_FLAG_* flags to choose i2c source clock here. */
+    };
+    esp_err_t err = i2c_param_config(i2c_master_port, &conf);
+    if (err != ESP_OK) {
+        return err;
+    }
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(i2c_master_init());
+	ssd1306_init();
+	xTaskCreate(task_ssd1306_display_clear, "clear_display_1", 1024 * 2, NULL, 0, NULL);
+	vTaskDelay(100/portTICK_PERIOD_MS);
+	xTaskCreate(task_ssd1306_display_logo,"display_logo", 1024 * 6, NULL, 0, NULL);
+}
